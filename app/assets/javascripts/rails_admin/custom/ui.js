33a889bb1488209adce115dc52e92aaf53c00316
Gabriele Tassoni <gabriele.tassoni@gmail.com>
2020-05-14 19:27:27 +0200

    place the require on engine in the right place

--- app/assets/javascripts/rails_admin/custom/ui.js
+++ app/assets/javascripts/rails_admin/custom/ui.js
@@ -1,18 +1,4 @@
-//= require selectize
 //= require timer
-//= require froala_editor.min.js
-//= require plugins/align.min.js
-//= require plugins/char_counter.min.js
-//= require plugins/colors.min.js
-//= require plugins/font_family.min.js
-//= require plugins/font_size.min.js
-//= require plugins/line_breaker.min.js
-//= require plugins/link.min.js
-//= require plugins/lists.min.js
-//= require plugins/special_characters.min.js
-//= require plugins/url.min.js
-//= require languages/it.js
-//= require rails_admin/custom/ckeditor_ajax
 //= require_tree .
 
 $(document).on('ready pjax:success', function(e) {
--- lib/thecore_ui_rails_admin.rb
+++ lib/thecore_ui_rails_admin.rb
@@ -1,5 +1,3 @@
-require "thecore_ui_rails_admin/engine"
-
 require 'thecore_ui_commons'
 # Rails Admin
 ENV['RAILS_ADMIN_THEME'] ||= 'rollincode'
@@ -12,6 +10,8 @@ require 'jquery-ui-rails'
 require 'bootstrap-sass'
 
 require 'concerns/thecore_ui_rails_admin_user'
+
+require "thecore_ui_rails_admin/engine"
 
 module ThecoreUiRailsAdmin
   # Your code goes here...

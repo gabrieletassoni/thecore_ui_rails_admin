1f9c965d888d12c4d59883d7df95e23a921ae92e
Gabriele Tassoni <gabrieletassoni@alchemic.it>
2021-08-27 09:46:47 +0000

    Add More level of customization through the use of overridable scss and js

--- app/assets/javascripts/rails_admin/custom/ui.js
+++ app/assets/javascripts/rails_admin/custom/ui.js
@@ -1,3 +1,8 @@
 //= require selectize
 //= require thecore_ui_commons/thecore
 //= require rails_admin/custom/thecore
+//= require rails_admin/custom/thecore-custom-1
+//= require rails_admin/custom/thecore-custom-2
+//= require rails_admin/custom/thecore-custom-3
+//= require rails_admin/custom/thecore-custom-4
+//= require rails_admin/custom/thecore-custom-5

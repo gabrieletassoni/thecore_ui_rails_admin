fa9915379284ea1a6c0e05f1abfee1bbc7031c0b
Gabriele Tassoni <gabrieletassoni@alchemic.it>
2021-09-01 04:56:01 +0000

    Add a way to hide breadcrumb and toolbar

--- app/assets/javascripts/rails_admin/custom/ui.js
+++ app/assets/javascripts/rails_admin/custom/ui.js
@@ -6,3 +6,23 @@
 //= require rails_admin/custom/thecore-custom-3
 //= require rails_admin/custom/thecore-custom-4
 //= require rails_admin/custom/thecore-custom-5
+
+function hideBreadCrumbAndToolbar() {
+    // If the page loaded has a tag like <span class="hide-breadcrumb hide-toolbar"></span>
+    // then hide the elements
+
+    // var actionName = "#{action_name}"
+    // var isRoot = "#{RailsAdmin::Config::Actions.find(action_name.to_sym).root?}"
+
+    // console.log(actionName, isRoot)
+
+    if($(".hide-breadcrumb").length) $(".breadcrumb").hide()
+    else $(".breadcrumb").show()
+
+    if($(".hide-toolbar").length) $(".breadcrumb + .nav.nav-tabs").hide()
+    else $(".breadcrumb + .nav.nav-tabs").show()
+}
+
+// Re evaluate at each iteraction
+$(document).off('ready pjax:success', hideBreadCrumbAndToolbar);
+$(document).on('ready pjax:success', hideBreadCrumbAndToolbar);

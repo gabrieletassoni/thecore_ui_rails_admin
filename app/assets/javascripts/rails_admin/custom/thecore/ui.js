c49e93e1db816d856f146c8fc747277269313000
Gabriele Tassoni <g.tassoni@bancolini.com>
2023-02-13 08:20:54 +0000

    Debug assets precompile chain

--- app/assets/javascripts/rails_admin/custom/thecore/ui.js
+++ app/assets/javascripts/rails_admin/custom/thecore/ui.js
@@ -1,1 +1,2 @@
 // override this file in your thecore based application to add custom behaviour
+console.log("Is this overridden")
--- app/assets/javascripts/rails_admin/custom/ui.js
+++ app/assets/javascripts/rails_admin/custom/ui.js
@@ -7,15 +7,4 @@ const adjustIframe = function(obj) {
     obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
 };
 
-// // Re evaluate at each iteration
-// $(document).on('rails_admin.dom_ready', function () {
-//     // If the page loaded has a tag like <span class="hide-breadcrumb hide-toolbar"></span>
-//     // then hide the elements
-//     if ($(".hide-breadcrumb").length) $(".breadcrumb").hide()
-//     else $(".breadcrumb").show()
-
-//     if ($(".hide-toolbar").length) $(".breadcrumb + .nav.nav-tabs").hide()
-//     else $(".breadcrumb + .nav.nav-tabs").show()
-// });
-
 //= require 'rails_admin/custom/thecore/ui'

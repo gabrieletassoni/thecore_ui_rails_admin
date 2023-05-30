c96d75cbc6271cd810d7d2226030490e411f85ee
Gabriele Tassoni <g.tassoni@bancolini.com>
2023-05-30 09:12:51 +0000

    Version Bump

--- lib/thecore_ui_rails_admin/version.rb
+++ lib/thecore_ui_rails_admin/version.rb
@@ -1,3 +1,4 @@
 module ThecoreUiRailsAdmin
-  VERSION = "#{`git describe --tags $(git rev-list --tags --max-count=1)`.chomp}"
+  VERSION = "3.1.4".freeze
+  # "#{`git describe --tags $(git rev-list --tags --max-count=1)`.chomp}"
 end

c1a435f95d13f8ec44aa97db10914691d614670b
Gabriele Tassoni <g.tassoni@bancolini.com>
2024-07-22 11:01:01 +0000

    Move here rails admin specifi ability

--- config/initializers/abilities.rb
+++ config/initializers/abilities.rb
@@ -2,6 +2,8 @@ module Abilities
     class ThecoreUiRailsAdmin
         include CanCan::Ability
         def initialize user
+            can :access, :rails_admin   # grant access to rails_admin
+            can :read, :dashboard       # grant access to the dashboard
             # No one is allowed to add or destroy settings
             # just list or edit existing
             cannot [:create, :destroy, :show], ThecoreSettings::Setting
--- lib/thecore_ui_rails_admin/version.rb
+++ lib/thecore_ui_rails_admin/version.rb
@@ -1,3 +1,3 @@
 module ThecoreUiRailsAdmin
-  VERSION = "3.2.4".freeze
+  VERSION = "3.2.5".freeze
 end

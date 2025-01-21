8268c855e97af6f22735cc19262345bff054c0f3
Gabriele Tassoni <g.tassoni@bancolini.com>
2025-01-21 13:59:41 +0000

    3.2.11

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  # scope ENV.fetch("RAILS_RELATIVE_URL_ROOT", "/") do
-  #   mount RailsAdmin::Engine => "/app", as: 'rails_admin'
-  # end
-
   mount RailsAdmin::Engine => "#{ENV.fetch('RAILS_RELATIVE_URL_ROOT', '')}/app", as: 'rails_admin'
 end
--- lib/thecore_ui_rails_admin/version.rb
+++ lib/thecore_ui_rails_admin/version.rb
@@ -1,3 +1,3 @@
 module ThecoreUiRailsAdmin
-  VERSION = "3.2.10".freeze
+  VERSION = "3.2.11".freeze
 end

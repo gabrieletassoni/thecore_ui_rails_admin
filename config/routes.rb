ffd0c4dd7a1c96a6fde8389e0e62265f2861ad6c
Gabriele Tassoni <g.tassoni@bancolini.com>
2025-01-30 15:03:13 +0000

    Fix route mount

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => "#{ENV.fetch('RAILS_RELATIVE_URL_ROOT', '')}/app", as: 'rails_admin'
+  mount RailsAdmin::Engine => "#{ENV.fetch('RAILS_RELATIVE_URL_ROOT', '/')}/app".gsub('//', '/'), as: 'rails_admin'
 end

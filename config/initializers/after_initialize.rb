0fecbe50831714c70518d7fe3026f8defd06ebed
Gabriele Tassoni <g.tassoni@bancolini.com>
2025-12-16 11:24:09 +0000

    LDAP Server: finalize tester with print of user information and disable bulk import of LDAP users from the UI

--- config/initializers/after_initialize.rb
+++ config/initializers/after_initialize.rb
@@ -1,56 +1,56 @@
 Rails.application.configure do
-    config.after_initialize do
-        puts "ThecoreUiRailsAdmin after_initialize"
+  config.after_initialize do
+    puts "ThecoreUiRailsAdmin after_initialize"
 
-        RailsAdmin::ApplicationController.send(:include, ConcernCommonApplicationController)
-        ApplicationController.send(:include, ConcernRAApplicationController)
-        RailsAdmin::ApplicationController.send(:include, ConcernRAApplicationController)
-        ## Rails Admin
-        require 'rails_admin_abstract_controller'
-        RailsAdmin::Config.parent_controller = '::RailsAdminAbstractController'
-        ### More at https://github.com/sferik/rails_admin/wiki/Base-configuration
-        ## == Devise ==
-        RailsAdmin::Config.authenticate_with do 
-            warden.authenticate! scope: :user 
-        end
-        RailsAdmin::Config.current_user_method(&:current_user)
-
-        ## == Cancan ==
-        RailsAdmin::Config.authorize_with :cancancan
-
-        RailsAdmin::Config.main_app_name = Proc.new { |controller| [ ((ThecoreSettings::Setting.where(ns: :main, key: :app_name).pluck(:raw).first.presence || ENV["APP_NAME"]) rescue "Thecore"), "" ] }
-
-        RailsAdmin::Config.show_gravatar = false
-
-        RailsAdmin::Config.label_methods.unshift(:display_name)
-
-        RailsAdmin::Config.excluded_models << ActionText::RichText
-        RailsAdmin::Config.excluded_models << ActionText::EncryptedRichText
-        RailsAdmin::Config.excluded_models << ActiveStorage::Blob
-        RailsAdmin::Config.excluded_models << ActiveStorage::Attachment
-        RailsAdmin::Config.excluded_models << ActiveStorage::VariantRecord
-        RailsAdmin::Config.excluded_models << ActionMailbox::InboundEmail
-        RailsAdmin::Config.excluded_models << UsedToken rescue puts "No UsedToken Model it could be normal: maybe model_driven_api is not installed"
-
-        RailsAdmin::Config::Actions::Export.send(:include, ExportConcern)
-        RailsAdmin::Config::Actions::BulkDelete.send(:include, BulkDeleteConcern)
-
-        Role.send :include, ThecoreUiRailsAdminRoleConcern
-        User.send :include, ThecoreUiRailsAdminUserConcern
-        RoleUser.send :include, ThecoreUiRailsAdminRoleUserConcern
-        Action.send :include, ThecoreUiRailsAdminActionConcern
-        PermissionRole.send :include, ThecoreUiRailsAdminPermissionRoleConcern
-        Permission.send :include, ThecoreUiRailsAdminPermissionConcern
-        Predicate.send :include, ThecoreUiRailsAdminPredicateConcern
-        Target.send :include, ThecoreUiRailsAdminTargetConcern
-        ThecoreSettings::Setting.send :include, ThecoreUiRailsAdminSettingsConcern
-
-        require 'root_actions/general_computation'
-        require 'root_actions/active_job_monitor'
-        require 'member_actions/change_password'
-        require 'member_actions/test_ldap_server'
-        require 'member_actions/import_users_from_ldap'
-        require 'collection_actions/save_filters'
-        require 'collection_actions/load_filters'
+    RailsAdmin::ApplicationController.send(:include, ConcernCommonApplicationController)
+    ApplicationController.send(:include, ConcernRAApplicationController)
+    RailsAdmin::ApplicationController.send(:include, ConcernRAApplicationController)
+    ## Rails Admin
+    require "rails_admin_abstract_controller"
+    RailsAdmin::Config.parent_controller = "::RailsAdminAbstractController"
+    ### More at https://github.com/sferik/rails_admin/wiki/Base-configuration
+    ## == Devise ==
+    RailsAdmin::Config.authenticate_with do
+      warden.authenticate! scope: :user
     end
+    RailsAdmin::Config.current_user_method(&:current_user)
+
+    ## == Cancan ==
+    RailsAdmin::Config.authorize_with :cancancan
+
+    RailsAdmin::Config.main_app_name = Proc.new { |controller| [((ThecoreSettings::Setting.where(ns: :main, key: :app_name).pluck(:raw).first.presence || ENV["APP_NAME"]) rescue "Thecore"), ""] }
+
+    RailsAdmin::Config.show_gravatar = false
+
+    RailsAdmin::Config.label_methods.unshift(:display_name)
+
+    RailsAdmin::Config.excluded_models << ActionText::RichText
+    RailsAdmin::Config.excluded_models << ActionText::EncryptedRichText
+    RailsAdmin::Config.excluded_models << ActiveStorage::Blob
+    RailsAdmin::Config.excluded_models << ActiveStorage::Attachment
+    RailsAdmin::Config.excluded_models << ActiveStorage::VariantRecord
+    RailsAdmin::Config.excluded_models << ActionMailbox::InboundEmail
+    RailsAdmin::Config.excluded_models << UsedToken rescue puts "No UsedToken Model it could be normal: maybe model_driven_api is not installed"
+
+    RailsAdmin::Config::Actions::Export.send(:include, ExportConcern)
+    RailsAdmin::Config::Actions::BulkDelete.send(:include, BulkDeleteConcern)
+
+    Role.send :include, ThecoreUiRailsAdminRoleConcern
+    User.send :include, ThecoreUiRailsAdminUserConcern
+    RoleUser.send :include, ThecoreUiRailsAdminRoleUserConcern
+    Action.send :include, ThecoreUiRailsAdminActionConcern
+    PermissionRole.send :include, ThecoreUiRailsAdminPermissionRoleConcern
+    Permission.send :include, ThecoreUiRailsAdminPermissionConcern
+    Predicate.send :include, ThecoreUiRailsAdminPredicateConcern
+    Target.send :include, ThecoreUiRailsAdminTargetConcern
+    ThecoreSettings::Setting.send :include, ThecoreUiRailsAdminSettingsConcern
+
+    require "root_actions/general_computation"
+    require "root_actions/active_job_monitor"
+    require "member_actions/change_password"
+    require "member_actions/test_ldap_server"
+    # require 'member_actions/import_users_from_ldap' # Disabled as it's a bit risky to have it in the UI
+    require "collection_actions/save_filters"
+    require "collection_actions/load_filters"
+  end
 end
--- lib/member_actions/test_ldap_server.rb
+++ lib/member_actions/test_ldap_server.rb
@@ -10,38 +10,50 @@ RailsAdmin::Config::Actions.add_action "test_ldap_server", :base, :member do
   # Adding the controller which is needed to compute calls from the ui
   controller do
     proc do
+      @ldap = ::LdapServer.find(@object.id)
+      # begin
+      @ldap.test_connection
+      @status = "success"
+
+      @message = I18n.t("admin.actions.test_ldap_server.success")
+
       # From the UI the user can test if the ldap server is reachable and receive a response
-      if request.get? || request.post?
-        @ldap = ::LdapServer.find(@object.id)
-        # begin
-        @ldap.test_connection
-        @status = "success"
+      if request.xhr? && request.post? && params[:email].present? && params[:password].present?
+        Rails.logger.debug("LDAP Test: Attempting to authenticate user #{params[:email]}")
+        authenticator = Ldap::Authenticator.new(
+          email: params[:email],
+          password: params[:password],
+        )
+        @ldap_user = authenticator.auth_on_single_server(@ldap)
+        @ldap_attributes = {}
 
-        @message = I18n.t("admin.actions.test_ldap_server.success")
+        @ldap_user.each_attribute do |key, values|
+          safe_values = values.map do |v|
+            s = v.to_s
 
-        # If in the form an email and password are provided, try to authenticate
-        if params[:email].present? && params[:password].present?
-          Rails.logger.debug("LDAP Test: Attempting to authenticate user #{params[:email]}")
-          authenticator = Ldap::Authenticator.new(
-            email: params[:email],
-            password: params[:password],
-          )
-          @ldap_user = authenticator.auth_on_single_server(@ldap)
-          Rails.logger.debug("LDAP Test: Authentication result for user #{params[:email]}: #{@ldap_user.inspect}")
-          # If @ldap_user exists, authentication succeeded
-          if @ldap_user
-            @message += " " + I18n.t("admin.actions.test_ldap_server.auth_success", email: params[:email])
-          else
-            @message += " " + I18n.t("admin.actions.test_ldap_server.auth_failure", email: params[:email])
-            @status = "warning"
+            # 1. Declare UTF-8
+            s.force_encoding("UTF-8")
+
+            # 2. Replace invalid / undefined bytes
+            s.encode!("UTF-8", invalid: :replace, undef: :replace, replace: "�")
+
+            s
           end
+
+          @ldap_attributes[key] = safe_values
         end
+        Rails.logger.debug("LDAP Test: Authentication result for user #{params[:email]}: #{@ldap_user.inspect}")
+        if @ldap_user
+          @message += " " + I18n.t("admin.actions.test_ldap_server.auth_success", email: params[:email])
+        else
+          @message += " " + I18n.t("admin.actions.test_ldap_server.auth_failure", email: params[:email])
+          @status = "warning"
+        end
+        # else
         # rescue => e
         #   @message = I18n.t("admin.actions.test_ldap_server.error", error: e.message)
         #   @status = "danger"
         # end
-        # Redirect to the object
-        # redirect_to index_path(model_name: @abstract_model.to_param)
       end
     end
   end

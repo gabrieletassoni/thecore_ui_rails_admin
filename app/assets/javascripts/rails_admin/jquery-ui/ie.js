63335f42ac0eb0390a927f9c94eac83004f00d02
Gabriele Tassoni <g.tassoni@bancolini.com>
2026-01-22 10:21:37 +0000

    Add fix also in app/assets

---
+++ app/assets/javascripts/rails_admin/jquery-ui/ie.js
@@ -0,0 +1,2 @@
+// Override to remove the dependency on ie.js from rails_admin which introduces the usage of navigator.userAgent.toLowerCase()
+// This file is intentionally left blank to avoid loading ie.js
--- lib/thecore_ui_rails_admin/version.rb
+++ lib/thecore_ui_rails_admin/version.rb
@@ -1,3 +1,3 @@
 module ThecoreUiRailsAdmin
-  VERSION = "3.5.7".freeze
+  VERSION = "3.5.8".freeze
 end
